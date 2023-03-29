a4833a571ea853d47757811d31a80f0e440d5118
laylarodas <rodas.layla@gmail.com>
2023-03-29 11:19:58 +0200

    create y delete- api

--- controllers/api/runnerControllerAPI.js
+++ controllers/api/runnerControllerAPI.js
@@ -5,3 +5,19 @@ exports.runner_list = function (req,res) {
         runners: Runner.allRunners
     })
 }
+
+exports.runner_create = function(req, res){
+    var runner = new Runner(req.body.id, req.body.gender, req.body.age);
+    runner.location = [req.body.lat, req.body.lng];
+    Runner.add(runner);
+
+    res.status(200).json({
+        runner: runner
+    });
+}
+
+
+exports.runner_delete = function(req, res){
+    Runner.removeById(req.body.id);
+    res.status(204).send();
+}
--- routes/api/runners.js
+++ routes/api/runners.js
@@ -5,5 +5,8 @@ var runnerController = require('../../controllers/api/runnerControllerAPI');
 
 router.get('/', runnerController.runner_list);
 
+router.post('/create', runnerController.runner_create);
+router.delete('/delete', runnerController.runner_delete);
+
 
 module.exports = router;
